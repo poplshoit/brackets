30e6df790273212c26356dc0cdcb5e486c38e6ec
Maksim Popushoi <poplshoy@gmail.com>
2020-10-19 21:12:19 +0300

    feat: end brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = bracketsConfig.map((config) => config[0]);
+  const closesBrackets = bracketsConfig.map((config) => config[1]);
+  const stack = [];
+
+  for (let i = 0; i <str.length; i++) {
+    const currentSymbol = str[i];
+    if (currentSymbol === '|' || currentSymbol === '7' || currentSymbol === '8') {
+      if (stack.indexOf(currentSymbol) != -1) {
+        stack.pop()
+      } else {
+      stack.push(currentSymbol)
+    }
+  }
+
+  if (openBrackets.indexOf(currentSymbol) != -1) {
+      stack.push(currentSymbol);
+  }
+
+  if (closesBrackets.indexOf(currentSymbol) != -1) {
+    const lastStackBracket = stack.pop();
+
+    const openIndex = openBrackets.indexOf(lastStackBracket);
+    const closeIndex = closesBrackets.indexOf(currentSymbol);
+    if ( openIndex !== closeIndex ) {
+      return false;
+    }
+  }
+}
+  return !stack.length 
 }
